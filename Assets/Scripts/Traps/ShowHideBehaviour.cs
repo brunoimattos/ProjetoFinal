7d817492122eec3b643e71b92cfb2bc3059c25b3
Bruno Mattos <brunoimattos@gmail.com>
2013-04-11 00:06:58 -0300

    Começando protótipo laser.
    Card #24.

---
+++ Assets/Scripts/Traps/ShowHideBehaviour.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class ShowHideBehaviour : MonoBehaviour
+{
+
+	
+	public float showTime;
+	public float hideTime;
+	public Transform showHideObject;
+	
+	void Start()
+	{
+		StartCoroutine(showObject(showTime));
+	}
+	
+	IEnumerator showObject(float showTime)
+	{
+		showHideObject.renderer.enabled = true;
+		
+		yield return new WaitForSeconds(showTime);
+		
+		StartCoroutine(hideObject(hideTime));
+	}
+	
+	IEnumerator hideObject(float hideTime)
+	{
+		showHideObject.renderer.enabled = false;
+		yield return new WaitForSeconds(hideTime);
+		
+		StartCoroutine(showObject(showTime));
+	}
+}
