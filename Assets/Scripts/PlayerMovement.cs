907162e9a35e26182f53b13bb1e67bbd64984608
Bruno Mattos <brunoimattos@gmail.com>
2013-08-20 05:04:35 -0300

    Export para Android e Movimento Jogador mobile.
    Atendendo parte do card #28.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,55 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
 	public float playerSpeed;
+	private Vector2 touchDeltaPosition;
+	private delegate void DoMovement();
+	private DoMovement doMovement;
 	
 	void Start()
 	{
-
+		#if UNITY_ANDROID
+			doMovement += doAndroidMovement;			
+		#endif
+		
+		#if UNITY_EDITOR
+			doMovement += doPCMovement;
+		#endif
 	}
 
 	void Update()
 	{
-		doMovement();
+		if (doMovement != null)
+		{
+			doMovement();
+		}
 	}
 	
-	void doMovement()
+	void doPCMovement()
 	{
-		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0); 
-		
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 		this.transform.Translate(movement * playerSpeed * Time.deltaTime);
 		
 		//this.rigidbody.MovePosition( this.transform.position + (movement * playerSpeed * Time.deltaTime));
+	}
+	
+	void doAndroidMovement()
+	{
+		if (Input.touchCount > 0)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Moved)
+			{
+				touchDeltaPosition -= Input.GetTouch(0).deltaPosition;
+				touchDeltaPosition.Normalize();
+			}
+			
+			this.transform.Translate(-touchDeltaPosition.x * playerSpeed * Time.deltaTime, -touchDeltaPosition.y * playerSpeed * Time.deltaTime, 0);
+		}
+		
+		if (Input.touchCount == 0)
+		{
+			touchDeltaPosition.x = 0.0f;			
+			touchDeltaPosition.y = 0.0f;			
+		}
 	}
 	
 	
