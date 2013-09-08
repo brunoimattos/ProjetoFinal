e00b0c4877336b2c476e6da3fd847278c4abb32a
Luiz Felipe da Costa Pericolo Barbosa <luizpericolo@gmail.com>
2013-09-08 17:42:23 -0300

    Defining the behaviour of the laser blast. It explodes with a certain distance. We could make it explode on contact, but naaah!

---
+++ Assets/Scripts/Traps/TurretLaserBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretLaserBehaviour : MonoBehaviour {
+
+	private bool startCounting;
+	private Vector3 origin = Vector3.zero;
+	
+	public float bulletRange = -1.0f;
+	
+	void Awake() 
+	{
+		if(bulletRange < 0.0f)
+		{
+			Debug.LogError("Please set bulletRange.");
+		}
+	}
+	
+	
+	void Update()
+	{	
+		if(this.origin != Vector3.zero)
+		{
+			if(Vector3.Distance(origin, transform.position) >= bulletRange)
+			{
+				Destroy(this.gameObject);
+			}	
+		}
+		
+	}
+	
+	public void setOrigin(Vector3 origin)
+	{
+		this.origin = origin;
+	}
+}
