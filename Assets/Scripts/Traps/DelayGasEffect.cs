2be0bc08a59cc9393c3a67d9b8bcf5ea0659ee7a
Luiz Felipe da Costa Pericolo Barbosa <luizpericolo@gmail.com>
2013-09-08 19:47:26 -0300

    Skeleton for the effect of the Delay Gas.

---
+++ Assets/Scripts/Traps/DelayGasEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayGasEffect : MonoBehaviour {
+
+	private bool active;
+	
+	void Awake () {
+		active = false;
+	}
+	
+	void Update () {
+		if(active)
+		{
+			Debug.Log("OMG! DELAYED! DELAYED!");	
+		}
+		
+	}
+	
+	public void activateEffect()
+	{
+		this.active = true;
+	}
+}
