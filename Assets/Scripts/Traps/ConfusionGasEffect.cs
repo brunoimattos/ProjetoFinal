77665020666a76eb467891d291a9bf82d552f3dc
Luiz Felipe da Costa Pericolo Barbosa <luizpericolo@gmail.com>
2013-09-15 16:47:23 -0300

    Finished creating the effect for the confuse gas.

--- Assets/Scripts/Traps/ConfusionGasEffect.cs
+++ Assets/Scripts/Traps/ConfusionGasEffect.cs
@@ -4,23 +4,51 @@ using System.Collections;
 public class ConfusionGasEffect : MonoBehaviour 
 {
 	public float confuseTime = 3.0f;
+
+	
+	private Texture2D confuseEffect;
 	private PlayerMovement playerApi;
+	
+	private float alphaDecrease;
+	private float alpha;
 	
 	void Start()
 	{
 		if(confuseTime < 0.0f)
 		{
-			Debug.LogError("Please set confuseTime");
+			Debug.LogError("Please set confuseTime!");
 		}
 		
 		playerApi = GetComponent<PlayerMovement>();
 		
 		if(playerApi == null)
 		{
-			Debug.LogError("Eitcha lele!");
+			Debug.LogError("Eitcha lele! Cade o PlayerMovement no player?");
 		}
 		
+		ResourceManager resourceApi = GameObject.FindGameObjectWithTag("ResourceManager").GetComponent<ResourceManager>();
+		
+		confuseEffect = resourceApi.getTextureByName("confuse_gas_effect");
+		
+		Debug.Log("Texturaaaa: " + confuseEffect.name);
+		
+		alpha = GUI.color.a;
+		
+		// Agora nao me pergunte pq o confuseTime ao quadrado =P
+		alphaDecrease = ( alpha / (confuseTime * confuseTime ) ) * Time.deltaTime;
+		Debug.Log("Decrease: " + alphaDecrease);
+		
 		StartCoroutine(doConfusion(confuseTime, playerApi));
+	}
+	
+	void createGuiTexture(GameObject guiTex, Texture2D texture)
+	{
+		guiTex = new GameObject("ConfuseGasEffect");
+		guiTex.AddComponent("GUITexture");
+		guiTex.transform.localScale = new Vector3(1.0f, 1.0f, 0);
+		guiTex.transform.position = new Vector3(0.5f, 0.5f, 0);
+		guiTex.guiTexture.pixelInset = new Rect(0, 0, 0, 0);
+		guiTex.guiTexture.texture = texture;
 	}
 	
 	IEnumerator doConfusion(float time, PlayerMovement playerApi)
@@ -31,11 +59,19 @@ public class ConfusionGasEffect : MonoBehaviour
 		
 		playerApi.setConfusion(false);
 		Destroy(this);
+		Destroy(guiText);
 		
 	}
 	
 	void OnGUI()
 	{
-		GUI.Label(new Rect(500, 10, 100, 25), "To confuso!");
+		
+		// Mudando o alpha da imagem antes de desenhar ela.
+		Color c = GUI.color;
+		alpha -= alphaDecrease;
+		c.a = alpha;
+		GUI.color = c;
+
+		GUI.DrawTexture(new Rect(0f, 0f, Screen.width, Screen.height), confuseEffect);
 	}
 }
