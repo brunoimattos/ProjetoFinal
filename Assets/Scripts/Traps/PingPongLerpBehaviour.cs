4ab6b5e3ed51bd8cceb8617bd21e5a796a0333b4
Luiz Felipe da Costa Pericolo Barbosa <luizpericolo@gmail.com>
2013-10-27 16:52:30 -0200

    Script que implementa movimento PingPong usando lerp.

---
+++ Assets/Scripts/Traps/PingPongLerpBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPongLerpBehaviour : MonoBehaviour {
+	
+	private Vector3 _fromPosition;
+	private Vector3 _toPosition;
+ 	
+    IEnumerator Start()
+    {
+		_fromPosition = transform.position;
+		_toPosition = _fromPosition + Vector3.forward * 0.5f;
+		
+		while (true) {
+		    yield return StartCoroutine(MoveObject(transform, _fromPosition, _toPosition, 0.2f));
+			yield return new WaitForSeconds(1.0f);
+		    yield return StartCoroutine(MoveObject(transform, _toPosition, _fromPosition, 2.0f));
+		}
+    }
+ 
+    IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+		var i= 0.0f;
+		var rate= 1.0f/time;
+		while (i < 1.0f) {
+			i += Time.deltaTime * rate;
+			thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+			yield return null; 
+		}
+    }
+	
+}
+
