f8b77b666906b00bc3a82a541c695d75f70fb64c
Luiz Felipe da Costa Pericolo Barbosa <luizpericolo@gmail.com>
2013-10-27 17:47:16 -0200

    Tirando o iTween do movimento PingPong.

--- Assets/Scripts/Traps/PingPongMovementBehaviour.cs
+++ Assets/Scripts/Traps/PingPongMovementBehaviour.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class PingPongMovementBehaviour : MonoBehaviour
-{
+public class PingPongMovementBehaviour : MonoBehaviour{
 
 	
 	public Transform pointA;
@@ -10,33 +9,22 @@ public class PingPongMovementBehaviour : MonoBehaviour
 	public float speed;
 	public float movementTime;
 	
-	private Hashtable itweenParams;
-		
-	void Start()
-	{
-		if(pointA == null || pointB == null)
-			Debug.LogError("Please assign points for PingPong movement!");	
-		
-		this.transform.position = pointA.position;
-		
-		itweenParams = new Hashtable();
-		
-		itweenParams.Add("speed", speed);
-		itweenParams.Add("x", pointB.transform.position.x);
-		itweenParams.Add("y", pointB.transform.position.y);
-		itweenParams.Add("z", pointB.transform.position.z);
-		itweenParams.Add("easetype", iTween.EaseType.linear);
-		itweenParams.Add("looptype", iTween.LoopType.pingPong);
-		
-		iTween.MoveTo(this.transform.gameObject, itweenParams);
-		
-	}
 	
-	void Update()
-	{
-		/*float distance = (pointB.position - pointA.position).magnitude;
-		
-		this.transform.position = pointA.transform.position + Vector3.right * Mathf.PingPong(speed * Time.time, distance);*/
-	}
-
+	IEnumerator Start(){	
+		while (true) {
+		    yield return StartCoroutine(MoveObject(transform, pointA.position, pointB.position, movementTime));
+			
+		    yield return StartCoroutine(MoveObject(transform, pointB.position, pointA.position, movementTime));
+		}
+    }
+    
+	IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time){
+		var i= 0.0f;
+		var rate= 1.0f/time;
+		while (i < 1.0f) {
+			i += Time.deltaTime * rate;
+			thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+			yield return null; 
+		}
+    }
 }
