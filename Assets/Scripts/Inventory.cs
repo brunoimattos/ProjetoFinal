7de419cf6375d376d4add27e725bcc5969947e05
Luiz Felipe da Costa Pericolo Barbosa <luizpericolo@gmail.com>
2013-11-10 14:23:51 -0200

    Script de inventario.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+	private List<string> items;
+	
+	void Start () {
+		items = new List<string>();
+	}
+	
+	public bool HasItem(string name){
+		if(items.Contains(name)){
+			return true;
+		}
+		return false;
+	}
+	
+	public void AddItem(string name){
+		items.Add(name);
+		
+		Debug.Log("Items in inventory: ");
+		foreach(string item in items)
+			Debug.Log(item);
+	}
+}
