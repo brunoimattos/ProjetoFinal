a6c90e0a8dfac35236035da4d1e216bf77e4c7a1
Bruno Mattos <brunoimattos@gmail.com>
2014-02-26 12:53:02 -0300

    Main menu working.
    Falta fazer a tela dos controles.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,12 @@ public class MainMenu : MonoBehaviour {
 	
 	public GameObject mainPanelObject;
     public GameObject aboutPanelObject;
+	public GameObject optionsPanelObject;
 	
 	public void Awake(){
-		//NGUITools.SetActive(aboutPanelObject, false);
-		//NGUITools.SetActive(mainPanelObject, true);
+		NGUITools.SetActive(aboutPanelObject, false);
+		NGUITools.SetActive(optionsPanelObject, false);
+		NGUITools.SetActive(mainPanelObject, true);
 	}
 	
 	public void OnPlayButtonClick () {
@@ -16,12 +18,21 @@ public class MainMenu : MonoBehaviour {
 	}
 	
 	public void OnAboutButtonClick () {
+		Debug.Log("About Click.");
 		NGUITools.SetActive(mainPanelObject, false);
 		NGUITools.SetActive(aboutPanelObject, true);
 	}
+
+	public void OnOptionsButtonClick () {
+		Debug.Log("Options Click.");
+		NGUITools.SetActive(mainPanelObject, false);
+		NGUITools.SetActive(optionsPanelObject, true);
+	}
 	
 	public void OnBackButtonClick () {
+		Debug.Log("Back Click.");
 		NGUITools.SetActive(aboutPanelObject, false);
+		NGUITools.SetActive(optionsPanelObject, false);
 		NGUITools.SetActive(mainPanelObject, true);	
 	}
 }
